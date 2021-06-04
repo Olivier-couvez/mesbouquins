fe2a0394c7ea89c8faf4103ae4bcb4c96f15d46b
Olivier Couvez <o.couvez@free.fr>
2021-06-04 16:56:18 +0200

    livror affichage livre

---
+++ Base/livreor/bdd/bddconfig.php
@@ -0,0 +1,5 @@
+<?php
+$bddserver =  "192.168.1.109"; // "secuoli.freeboxos.fr"; // "82.64.227.23"; //"192.168.0.50"; // $bddserver = "localhost";
+$bddname = "mesbouquins";
+$bddlogin = "olivier";
+$bddpass = "olivier";
