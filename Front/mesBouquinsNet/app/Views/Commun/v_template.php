3d23f332c06f108ff82128a5c0f34147420acf6b
“Marion5950” <marion.brasme@edmond-labbe.fr>
2021-06-11 13:30:27 +0200

    css modified

--- Front/mesBouquinsNet/app/Views/Commun/v_template.php
+++ Front/mesBouquinsNet/app/Views/Commun/v_template.php
@@ -9,6 +9,10 @@
         <!-- <meta name="robots" content="none"> -->
         <link rel="stylesheet" href="<?= base_url("css/conteneurs.css")?>">
         <title>Points de collecte du Relais</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <script>
             // 100vh chrome mobile
             // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
