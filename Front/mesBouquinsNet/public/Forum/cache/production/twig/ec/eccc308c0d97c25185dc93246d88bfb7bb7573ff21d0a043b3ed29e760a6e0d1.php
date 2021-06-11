819b45eccb21be6623fb37a54e2a0056953397e5
“Marion5950” <marion.brasme@edmond-labbe.fr>
2021-06-11 11:35:58 +0200

    Forum style, forum images, logo site, lien vers forum template

---
+++ Front/mesBouquinsNet/public/Forum/cache/production/twig/ec/eccc308c0d97c25185dc93246d88bfb7bb7573ff21d0a043b3ed29e760a6e0d1.php
@@ -0,0 +1,239 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* navbar_footer.html */
+class __TwigTemplate_03e4520cc57760b532e5f3c9526e294db512a0b6e19c472c4be12849350edfdb extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"nav-footer_border\">
+\t<div class=\"navbar\" role=\"navigation\">
+\t\t<div class=\"inner\">
+
+\t\t<ul id=\"nav-footer\" class=\"nav-footer linklist\" role=\"menubar\">
+\t\t\t<li class=\"breadcrumbs\">
+\t\t\t\t";
+        // line 7
+        if (($context["U_SITE_HOME"] ?? null)) {
+            // line 8
+            echo "\t\t\t\t\t";
+            ob_start(function () { return ''; });
+            // line 9
+            echo "\t\t\t\t\t<span class=\"crumb\">
+\t\t\t\t\t\t<a href=\"";
+            // line 10
+            echo ($context["U_SITE_HOME"] ?? null);
+            echo "\" data-navbar-reference=\"home\">
+\t\t\t\t\t\t\t<i class=\"icon fa-home fa-fw\" aria-hidden=\"true\"></i><span>";
+            // line 11
+            echo $this->extensions['phpbb\template\twig\extension']->lang("SITE_HOME");
+            echo "</span>
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</span>
+\t\t\t\t\t";
+            $___internal_ec98c4c14ba5356befe0449da36d388ce9fbaef14db36628caf3c7bc050e93b9_ = ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+            // line 8
+            echo twig_spaceless($___internal_ec98c4c14ba5356befe0449da36d388ce9fbaef14db36628caf3c7bc050e93b9_);
+            // line 15
+            echo "\t\t\t\t";
+        }
+        // line 16
+        echo "\t\t\t\t";
+        // line 17
+        echo "\t\t\t\t";
+        ob_start(function () { return ''; });
+        // line 18
+        echo "\t\t\t\t<span class=\"crumb\">
+\t\t\t\t\t<a href=\"";
+        // line 19
+        echo ($context["U_INDEX"] ?? null);
+        echo "\" data-navbar-reference=\"index\">
+\t\t\t\t\t\t";
+        // line 20
+        if ( !($context["U_SITE_HOME"] ?? null)) {
+            echo "<i class=\"icon fa-home fa-fw\" aria-hidden=\"true\"></i>";
+        }
+        echo "<span>";
+        echo $this->extensions['phpbb\template\twig\extension']->lang("INDEX");
+        echo "</span>
+\t\t\t\t\t</a>
+\t\t\t\t</span>
+\t\t\t\t";
+        $___internal_33196890653fdb6a7a2911813cabcaf7ffcc3940c6f47d941e11e5ec5dd96d02_ = ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+        // line 17
+        echo twig_spaceless($___internal_33196890653fdb6a7a2911813cabcaf7ffcc3940c6f47d941e11e5ec5dd96d02_);
+        // line 24
+        echo "\t\t\t\t";
+        // line 25
+        echo "\t\t\t</li>
+\t\t\t";
+        // line 26
+        if ((($context["U_WATCH_FORUM_LINK"] ?? null) &&  !($context["S_IS_BOT"] ?? null))) {
+            // line 27
+            echo "\t\t\t\t<li data-last-responsive=\"true\">
+\t\t\t\t\t<a href=\"";
+            // line 28
+            echo ($context["U_WATCH_FORUM_LINK"] ?? null);
+            echo "\" title=\"";
+            echo ($context["S_WATCH_FORUM_TITLE"] ?? null);
+            echo "\" data-ajax=\"toggle_link\" data-toggle-class=\"icon ";
+            if (($context["S_WATCHING_FORUM"] ?? null)) {
+                echo "fa-check-square-o";
+            } else {
+                echo "fa-square-o";
+            }
+            echo " fa-fw\" data-toggle-text=\"";
+            echo ($context["S_WATCH_FORUM_TOGGLE"] ?? null);
+            echo "\" data-toggle-url=\"";
+            echo ($context["U_WATCH_FORUM_TOGGLE"] ?? null);
+            echo "\">
+\t\t\t\t\t\t<i class=\"icon ";
+            // line 29
+            if (($context["S_WATCHING_FORUM"] ?? null)) {
+                echo "fa-square-o";
+            } else {
+                echo "fa-check-square-o";
+            }
+            echo " fa-fw\" aria-hidden=\"true\"></i><span>";
+            echo ($context["S_WATCH_FORUM_TITLE"] ?? null);
+            echo "</span>
+\t\t\t\t\t</a>
+\t\t\t\t</li>
+\t\t\t";
+        }
+        // line 33
+        echo "\t\t\t";
+        // line 34
+        echo "
+\t\t\t";
+        // line 35
+        // line 36
+        echo "\t\t\t";
+        if ( !($context["S_IS_BOT"] ?? null)) {
+            // line 37
+            echo "\t\t\t\t<li class=\"rightside\">
+\t\t\t\t\t<a href=\"";
+            // line 38
+            echo ($context["U_DELETE_COOKIES"] ?? null);
+            echo "\" data-ajax=\"true\" data-refresh=\"true\" role=\"menuitem\">
+\t\t\t\t\t\t<i class=\"icon fa-trash fa-fw\" aria-hidden=\"true\"></i><span>";
+            // line 39
+            echo $this->extensions['phpbb\template\twig\extension']->lang("DELETE_COOKIES");
+            echo "</span>
+\t\t\t\t\t</a>
+\t\t\t\t</li>
+\t\t\t\t";
+            // line 42
+            if (($context["S_DISPLAY_MEMBERLIST"] ?? null)) {
+                // line 43
+                echo "\t\t\t\t\t<li class=\"rightside\" data-last-responsive=\"true\">
+\t\t\t\t\t\t<a href=\"";
+                // line 44
+                echo ($context["U_MEMBERLIST"] ?? null);
+                echo "\" title=\"";
+                echo $this->extensions['phpbb\template\twig\extension']->lang("MEMBERLIST_EXPLAIN");
+                echo "\" role=\"menuitem\">
+\t\t\t\t\t\t\t<i class=\"icon fa-group fa-fw\" aria-hidden=\"true\"></i><span>";
+                // line 45
+                echo $this->extensions['phpbb\template\twig\extension']->lang("MEMBERLIST");
+                echo "</span>
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</li>
+\t\t\t\t";
+            }
+            // line 49
+            echo "\t\t\t";
+        }
+        // line 50
+        echo "\t\t\t";
+        // line 51
+        echo "\t\t\t";
+        if (($context["U_TEAM"] ?? null)) {
+            // line 52
+            echo "\t\t\t\t<li class=\"rightside\" data-last-responsive=\"true\">
+\t\t\t\t\t<a href=\"";
+            // line 53
+            echo ($context["U_TEAM"] ?? null);
+            echo "\" role=\"menuitem\">
+\t\t\t\t\t\t<i class=\"icon fa-shield fa-fw\" aria-hidden=\"true\"></i><span>";
+            // line 54
+            echo $this->extensions['phpbb\template\twig\extension']->lang("THE_TEAM");
+            echo "</span>
+\t\t\t\t\t</a>
+\t\t\t\t</li>
+\t\t\t";
+        }
+        // line 58
+        echo "\t\t\t";
+        // line 59
+        echo "\t\t\t";
+        if (($context["U_CONTACT_US"] ?? null)) {
+            // line 60
+            echo "\t\t\t\t<li class=\"rightside\" data-last-responsive=\"true\">
+\t\t\t\t\t<a href=\"";
+            // line 61
+            echo ($context["U_CONTACT_US"] ?? null);
+            echo "\" role=\"menuitem\">
+\t\t\t\t\t\t<i class=\"icon fa-envelope fa-fw\" aria-hidden=\"true\"></i><span>";
+            // line 62
+            echo $this->extensions['phpbb\template\twig\extension']->lang("CONTACT_US");
+            echo "</span>
+\t\t\t\t\t</a>
+\t\t\t\t</li>
+\t\t\t";
+        }
+        // line 66
+        echo "\t\t</ul>
+\t\t</div>
+\t</div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "navbar_footer.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  214 => 66,  207 => 62,  203 => 61,  200 => 60,  197 => 59,  195 => 58,  188 => 54,  184 => 53,  181 => 52,  178 => 51,  176 => 50,  173 => 49,  166 => 45,  160 => 44,  157 => 43,  155 => 42,  149 => 39,  145 => 38,  142 => 37,  139 => 36,  138 => 35,  135 => 34,  133 => 33,  120 => 29,  104 => 28,  101 => 27,  99 => 26,  96 => 25,  94 => 24,  92 => 17,  81 => 20,  77 => 19,  74 => 18,  71 => 17,  69 => 16,  66 => 15,  64 => 8,  57 => 11,  53 => 10,  50 => 9,  47 => 8,  45 => 7,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "navbar_footer.html", "");
+    }
+}
