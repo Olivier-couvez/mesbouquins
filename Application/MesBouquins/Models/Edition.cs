3bfa77d1ac17f678aaee6d4e0869e506c7eb12a8
Olivier Couvez <o.couvez@free.fr>
2021-06-18 17:00:20 +0200

    ajout livre detail

--- Application/MesBouquins/Models/Edition.cs
+++ Application/MesBouquins/Models/Edition.cs
@@ -9,13 +9,13 @@ namespace MesBouquins.Models
     public class Edition
     {
         private Int16 id_Edition;
-        private string Edition_Nom;
-        private Int16 Id_Logo_Edition;
-        private string Site_Internet_Edition;
+        private string edition_Nom;
+        private Int16 id_Logo_Edition;
+        private string site_Internet_Edition;
 
         public short Id_Edition { get => id_Edition; set => id_Edition = value; }
-        public string Edition_Nom1 { get => Edition_Nom; set => Edition_Nom = value; }
-        public short Id_Logo_Edition1 { get => Id_Logo_Edition; set => Id_Logo_Edition = value; }
-        public string Site_Internet_Edition1 { get => Site_Internet_Edition; set => Site_Internet_Edition = value; }
+        public string Edition_Nom { get => edition_Nom; set => edition_Nom = value; }
+        public short Id_Logo_Edition { get => id_Logo_Edition; set => id_Logo_Edition = value; }
+        public string Site_Internet_Edition { get => site_Internet_Edition; set => site_Internet_Edition = value; }
     }
 }
