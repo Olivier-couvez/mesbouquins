0b342ec682a7caa1c3a89a38e037fbda48681095
Couvez olivier <o.couvez@free.fr>
2021-07-02 17:57:19 +0200

    maj bouquins

--- Base/livreor/bdd/bddconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-$bddserver =  "192.168.1.109"; // "secuoli.freeboxos.fr"; // "82.64.227.23"; //"192.168.0.50"; // $bddserver = "localhost";
-$bddname = "mesbouquins";
-$bddlogin = "olivier";
-$bddpass = "olivier";
